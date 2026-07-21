c51f13cdcb0f5f82891acebb520de635f242da42
CristopheGomes <cristophegomes33@gmail.com>
2026-07-21 15:17:53 -0300

    iniciando o codigo

--- JogoDaVelha/Program.cs
+++ JogoDaVelha/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void DesenharTabuleiro(char[,] tab)
+    {
+        
+    }
+    static void Main()
+    {
+        char[,] tabuleiro = new char[3, 3]
+        {
+            {' ', ' ', ' '},
+            {' ', ' ', ' '},
+            {' ', ' ', ' '}
+        };
+
+        char JogadorAtual = 'X';
+        bool jogoAtivo = true; 
+        int jogadasRealizadas = 0;
+
+
+        
+    }
+    
+}
